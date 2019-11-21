15ca31539039d028e70bc9d296c0192127c948da
Dark33535ege <45699787+Dark33535ege@users.noreply.github.com>
2019-11-21 17:01:45 +0300

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,16 +1,18 @@
 <?php
 $name       = @trim(stripslashes($_POST['name'])); 
-$from       = @trim(stripslashes($_POST['email']));
+$from       = @trim(stripslashes($_POST['email'])); 
+$subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'ismail.gugu@mail.ru';//replace with your email
+$to    = 'ismail.gugu@mail.ru';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
 $headers[] = "Content-type: text/plain; charset=iso-8859-1";
 $headers[] = "From: {$name} <{$from}>";
 $headers[] = "Reply-To: <{$from}>";
+$headers[] = "Subject: {$subject}";
 $headers[] = "X-Mailer: PHP/".phpversion();
 
-mail($to, $message, $headers);
-
+mail($to, $subject, $message, $headers);
+ 
 die;
